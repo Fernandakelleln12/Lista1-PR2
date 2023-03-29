f02879c175f185dc865183e5416a9d39ab849699
CJ3016595 <CJ3016595@LAB01-PC31>
2023-03-29 10:45:35 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1-PR2/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////exercicio 1
+            //int metros, cm, mm;
+            //Console.WriteLine("digite o numero de metros");
+            //metros = int.Parse( Console.ReadLine());
+            //cm = metros * 100;
+            //mm = metros * 1000;
+            //Console.WriteLine("a quantridade em cm é" + cm);  
+            //Console.WriteLine("a quantridade em mm é" + mm);
+            //Console.ReadKey();
+
+            ////exercicio 2
+            //int valor2, f1;
+            //float valor1, C;
+            //Console.WriteLine("Digite um  valor  em  graus  Fahrenheit");
+            //valor1 = int.Parse(Console.ReadLine());
+            //C = (valor1 - 32.0f) * (5.0f / 9.0f);
+
+            //Console.WriteLine("Digite um  valor  em  graus  Fahrenheit");
+            //valor2 = int.Parse(Console.ReadLine());
+            //f1 = (valor2 - 32) * (5 / 9);
+            //Console.WriteLine("resultado float" +C);
+            //Console.WriteLine("resulatdo int"+ f1 );
+            //Console.ReadKey();
+
+            ////exercicio 3
+            //int numDigitado1;
+            //float resultado, numDigitado2;
+
+            //Console.WriteLine("digite o peso");
+            //numDigitado1 = int.Parse(Console.ReadLine()); //LÊ o numero
+            //Console.WriteLine("digite a altura");
+            //numDigitado2 = float.Parse(Console.ReadLine()); //LÊ o numero
+
+            //resultado = (numDigitado2 * numDigitado2) / numDigitado1;
+            //Console.WriteLine(resultado);
+            //Console.ReadKey();
+
+            ////exercicio 4
+            //int numero1, numero2, numero3;
+            //float peso1, peso2, peso3, resultado, resultado2, resultado3;
+            //Console.WriteLine("digite o primeiro numero!");
+            //numero1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("digite o segundo numero");
+            //numero2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("digite o terceiroo numero");
+            //numero3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("digite o primeiro peso");
+            //peso1 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("digite o segundo peso");
+            //peso2 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("digite o terceiroo peso");
+            //peso3 = float.Parse(Console.ReadLine());
+
+            //resultado = (numero1 * peso1 + numero2 * peso2 + numero3 * peso3);
+            //resultado2 = (numero1 + numero2 + numero3);
+            //resultado3 = (resultado / resultado2);
+            //Console.WriteLine(resultado3);
+            //Console.ReadKey();
+
+            // exercicio 5
+
+            //float velocidade, tempo, espaço;
+
+            //Console.WriteLine("informe o tempo que o objeto demorou:");
+            //  tempo=float.Parse(Console.ReadLine());
+            //Console.WriteLine("informe o espaço percorido pelo objeto:");
+            // espaço=float.Parse(Console.ReadLine());
+
+            //velocidade= espaço/tempo;
+            //Console.WriteLine("a velocidade que o objeto estava é de "+velocidade);
+
+            //Console.ReadLine();
+
+
+
+            //exercicio 6
+
+           //loat salario, resultado, porcetagem;
+          //Console.WriteLine("digite o valor do salario");
+           //salario= float.Parse(Console.ReadLine());
+
+            //porcentagem = (salario * 25) / 100;
+            //resultado = salario + porcentagem;
+            //Console.WriteLine("o seu slario será"+resultado);
+
+            //Console.ReadLine();
+
+            //exercicio 7
+
+            //float real, dolar, cotação;
+
+            //Console.WriteLine("informe quantos reais vocé tem:");
+            //real = float.Parse(Console.ReadLine());
+            //Console.WriteLine("informe qual a cotação do dolar:");
+            //cotação = float.Parse(Console.ReadLine());
+
+            //dolar = real / cotação;
+
+            //Console.WriteLine("o seu dinheiro em dolar é"+dolar);
+
+            //exercicio 8
+
+            //float numero, susesorDOtriplo, antDOdobro, resultado;
+
+            // Console.WriteLine("digite um numero");
+            //numero=float.Parse(Console.ReadLine());
+
+            // susesorDOtriplo = (numero * 3) + 1;
+            //antDOdobro = (numero * 2) - 1;
+            //resultado = antDOdobro + susesorDOtriplo;
+
+            // Console.WriteLine("o resultado é"+ resultado);
+
+            //exercicio 9
+
+            //int num, d1, d2, d3, d4;
+
+            //Console.WriteLine("Digite um número inteiro de 4 dígitos:");
+            //num = int.Parse(Console.ReadLine());
+
+
+            //d1 = num / 1000;
+            //Console.WriteLine(d1);
+            //d2 = (num / 100) % 10;
+            //Console.WriteLine(d2);
+            //d3 = (num / 10) % 10;
+            //Console.WriteLine(d3);
+            //d4 = num % 10;
+            //Console.WriteLine(d4);
+
+            //Console.ReadLine();
+
+
+
+
+
+        }
+    }
+          
+
+    }
+
+    
+
